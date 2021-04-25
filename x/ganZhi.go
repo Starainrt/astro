ce2ae38ab8d9c85636b08f42cf926e910236d7d1
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-04-25 20:35:54 +0800

    月干支

--- x/ganZhi.go
+++ x/ganZhi.go
@@ -30,6 +30,10 @@ var (
 	Zhi = [13]string{"err", "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}
 )
 
+//##############################################s
+//计算年干支
+//##############################################
+
 //年干支
 func GetYGZ(year, month, day, hour int) string {
 	cust := time.Date(year, time.Month(month), day, hour, 0, 0, 0, time.Local) //精确到时
@@ -80,7 +84,7 @@ func fixLiChun(year int, cust time.Time) bool {
 	return b
 }
 
-//传入阳历年数字 返回本年立春阳历时间戳 12节时间戳数组
+//传入阳历年数字 返回本年立春阳历时间戳 12节时间戳数组(上一年冬至到本年冬至)
 //获取本年立春时间戳
 func getJie12T(year int) (time.Time, []time.Time) {
 
@@ -116,3 +120,127 @@ func getJie12T(year int) (time.Time, []time.Time) {
 	//排序后对应的k值 2 4 6 8 10 12 14 16 18 20 22 24
 	return lct, jieArr
 }
+
+//##############################################s
+//计算月干支
+//##############################################
+
+//传入阳历时间 返回月干支
+//以12节气定月干支
+func MonthGZ(year, month, day, hour int) string {
+	cust := time.Date(year, time.Month(month), day, hour, 0, 0, 0, time.Local)
+	arrT := getJieArr(year)
+	b, index := findJie(cust, arrT)
+	lcb := fixLiChun(year, cust)
+
+	yg, _ := YearGZ(year, lcb)
+	gzArr := mgzArr(yg)
+
+	if (b == false && index == 0) && lcb == false { //在本年立春之前
+		index -= 1
+		if index < 0 {
+			index += 12
+		}
+		index -= 1
+	} else if (b == false && index == 0) && lcb == true {
+		index -= 1
+		if index < 0 {
+			index += 12
+		}
+	} else if b == true {
+		index -= 1
+		if index < 0 {
+			index += 12
+		}
+	}
+	//fmt.Printf("年干:%s index:%d 月干支:%s\n", yg, index, gzArr[index])
+	return gzArr[index]
+}
+
+//正月立春节 二月惊蛰节 三月清明节 四月立夏节 五月忙钟节 六月小暑节
+//七月立秋节 八月白露节 九月寒露节 十月立东节 冬月大雪节 腊月小寒节
+//0:上一年小寒 1今年立春...11大雪 12本年小寒 13下年立春
+//上一年小寒到下一年节气的时间戳数组
+func getJieArr(year int) []time.Time {
+	_, j12arr := getJie12T(year)
+	_, j4arr := getJie12T(year + 1)
+
+	var arrT []time.Time
+	arrT = append(arrT, j12arr...)
+	arrT = append(arrT, j4arr...)
+	return arrT
+}
+
+//true节气之后 false节气之前 节气计算精确到日
+func findJie(cust time.Time, jarrT []time.Time) (bool, int) {
+	cust = time.Date(cust.Year(), cust.Month(), cust.Day(), 0, 0, 0, 0, time.Local)
+	var b bool
+	var index int
+	for i := 0; i < len(jarrT)-1; i++ {
+		j0 := jarrT[i]
+		j1 := jarrT[i+1]
+		j0 = time.Date(j0.Year(), j0.Month(), j0.Day(), 0, 0, 0, 0, time.Local) //精确到日
+		j1 = time.Date(j1.Year(), j1.Month(), j1.Day(), 0, 0, 0, 0, time.Local)
+		if (cust.Equal(j0) || cust.After(j0)) && cust.Before(j1) {
+			index = i
+			b = true //节气之后
+			break
+		}
+	}
+	return b, index
+}
+
+// 五虎盾元 甲己之年丙作初，乙庚之歲戊為頭，丙辛歲首從庚起，丁壬壬位順流行，若問戊癸何方法，甲寅之上好推求
+// 传入年干 返回本年月干支数组
+// 月干支数组
+func mgzArr(yg string) []string {
+	gan := []string{"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}
+	zhi := []string{"寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥", "子", "丑"} //正月建寅
+
+	var arr []string //月干支数组
+	switch yg {
+	case gan[0], gan[5]: //甲己 丙寅
+		end := gan[:2]
+		head := gan[2:]
+		arr = whd(gan, zhi, head, end)
+	case gan[1], gan[6]: //乙庚 戊寅
+		end := gan[:4]
+		head := gan[4:]
+		arr = whd(gan, zhi, head, end)
+	case gan[2], gan[7]: //丙辛 庚寅
+		end := gan[:6]
+		head := gan[6:]
+		arr = whd(gan, zhi, head, end)
+	case gan[3], gan[8]: //丁壬 壬寅
+		end := gan[:8]
+		head := gan[8:]
+		arr = whd(gan, zhi, head, end)
+	case gan[4], gan[9]: //戊癸 甲寅
+		end := gan
+		head := gan
+		arr = whd(gan, zhi, head, end)
+	}
+	return arr
+}
+
+//五虎遁干支数组
+func whd(gan, zhi, head, end []string) []string {
+	var arr []string
+	gan = append(head, end...)
+	gan = append(gan, gan...)
+	for i := 0; i < len(gan); i++ {
+		for j := i; j < len(zhi); j++ {
+			s := gan[j] + zhi[j]
+			arr = append(arr, s)
+			if j == i {
+				break
+			}
+
+		}
+	}
+	return arr
+}
+
+//##############################################s
+//计算日干支
+//##############################################
